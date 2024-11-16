1799679cbdd78a61bdfb55e50daca6797cbdb398
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-11-16 16:30:03 -0500

    ♻️ [refactor] components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { ReactNode } from 'react';
+import {
+	CreateTodoButton,
+	TodoCounter,
+	TodoItem,
+	TodoList,
+	TodoSearch,
+} from './components';
+
 import './App.css';
 
 function App() {
@@ -18,37 +25,5 @@ function App() {
 		</div>
 	);
 }
-
-interface TodoListType {
-	children: ReactNode;
-}
-
-const TodoList = ({ children }: TodoListType) => {
-	return <ul>{children}</ul>;
-};
-const CreateTodoButton = () => {
-	return <button>Create</button>;
-};
-
-const TodoCounter = () => {
-	return (
-		<h1>
-			You're completed {10} of {20} todo's
-		</h1>
-	);
-};
-const TodoSearch = () => {
-	return <input type='text' placeholder='Type todo' />;
-};
-
-const TodoItem = () => {
-	return (
-		<li>
-			<span>V</span>
-			<p>Todo example</p>
-			<span>X</span>
-		</li>
-	);
-};
 
 export default App;
