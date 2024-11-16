a4ce8ba15a1d19c1abc90eabc63f6e6044193db6
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-11-16 16:57:28 -0500

    🎉 [create] Todos default data

---
+++ src/data/Todos.ts
@@ -0,0 +1,21 @@
+import type { TodoType } from '../interfaces';
+
+const defaultTodos: TodoType[] = [
+	{
+		id: 1,
+		title: 'Todo 1',
+		completed: false,
+	},
+	{
+		id: 2,
+		title: 'Todo 2',
+		completed: false,
+	},
+	{
+		id: 3,
+		title: 'Todo 3',
+		completed: false,
+	},
+];
+
+export { defaultTodos };
