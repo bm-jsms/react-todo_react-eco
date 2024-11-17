538395cc635460d31ac4dc8cb51b9f65ec9d057f
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-17 03:56:29 -0500

    ✨ [add] onClick - alert test

--- src/components/CreateTodoButton.tsx
+++ src/components/CreateTodoButton.tsx
@@ -1,6 +1,9 @@
 const CreateTodoButton = () => {
 	return (
-		<button className='bg-primary text-white px-4 py-1 rounded-md border'>
+		<button
+			className='bg-primary text-white px-4 py-1 rounded-md border'
+			onClick={() => alert('Create Todo')}
+		>
 			Create
 		</button>
 	);
