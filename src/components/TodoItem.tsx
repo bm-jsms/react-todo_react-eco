5703ce3d24c04bd83a6bebb8d20d7c8c79c11637
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-17 04:49:33 -0500

    ✨ [add] methods

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,17 +1,25 @@
 import { TodoType } from '../interfaces';
 
-const TodoItem = ({ title, completed }: TodoType) => {
+const TodoItem = ({ title, completed, onComplete, onDelete }: TodoType) => {
 	return (
 		<li className='flex items-center w-full border justify-between px-5'>
 			<span
-				className={`text-xl ${completed && 'text-green-500'} font-semibold`}
+				className={`text-xl ${
+					completed && 'text-green-500'
+				} font-semibold cursor-pointer`}
+				onClick={onComplete}
 			>
 				V
 			</span>
 
 			<p className={`text-xl ${completed && 'line-through'}`}>{title}</p>
 
-			<span className='text-xl'>X</span>
+			<span
+				className='text-xl cursor-pointer hover:text-red-600'
+				onClick={onDelete}
+			>
+				X
+			</span>
 		</li>
 	);
 };
