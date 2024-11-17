c7e8eb7f88afb1acc435bd742383819a18dab0b5
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-17 05:00:39 -0500

    ♻️ [improve] Todo´s items

--- src/App.tsx
+++ src/App.tsx
@@ -39,23 +39,32 @@ function App() {
 	const totalTodos = todos.length;
 	const completedTodos = todos.filter((todo) => !!todo.completed).length;
 
+	const todoItemsProv = () => {
+		if (searchedTodos.length > 0) {
+			return searchedTodos.map(({ id, title, completed }) => (
+				<TodoItem
+					key={id}
+					title={title}
+					completed={completed}
+					onComplete={() => completeTodo(title)}
+					onDelete={() => deleteTodo(title)}
+				/>
+			));
+		}
+		return (
+			<li className='text-center text-xl font-medium text-slate-500/45'>
+				No todo's found
+			</li>
+		);
+	};
+
 	return (
 		<main className='flex flex-col items-center justify-center min-h-screen py-2 max-w-screen-md mx-auto'>
 			<TodoCounter completed={completedTodos} total={totalTodos} />
 
 			<TodoSearch searchValue={inputValue} setSearchValue={setInputValue} />
 
-			<TodoList>
-				{searchedTodos?.map(({ id, title, completed }) => (
-					<TodoItem
-						key={id}
-						title={title}
-						completed={completed}
-						onComplete={() => completeTodo(title)}
-						onDelete={() => deleteTodo(title)}
-					/>
-				))}
-			</TodoList>
+			<TodoList>{todoItemsProv()}</TodoList>
 
 			<CreateTodoButton />
 		</main>
