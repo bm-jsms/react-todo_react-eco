670907761fae9be7c7474d01f5fffe56c01afc30
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-17 20:15:06 -0500

    ✨ [add] Delete icon - react icons

--- src/icons/DeleteIcon.tsx
+++ src/icons/DeleteIcon.tsx
@@ -1,10 +1,13 @@
+import { MdDelete } from 'react-icons/md';
+
 import type { TodoIconHandle } from '../interfaces';
 
 const DeleteIcon = ({ handleClick }: TodoIconHandle) => {
 	return (
-		<span className='text-red-500' onClick={handleClick}>
-			X
-		</span>
+		<MdDelete
+			className='text-red-500/75 text-2xl cursor-pointer duration-150 hover:scale-x-110 hover:text-red-500 font-bold'
+			onClick={handleClick}
+		/>
 	);
 };
 
