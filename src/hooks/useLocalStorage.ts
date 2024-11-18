79710baaae2c134886de9ab4e18df6d406edb7fc
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-18 04:32:07 -0500

    ✨ [add] effects

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,19 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { TodoTypeData } from '../interfaces';
 
 const useLocalStorage = (itemName: string, initialValue: TodoTypeData[]) => {
-	const localeStorageItem = localStorage.getItem(itemName);
+	const [item, setItem] = useState(initialValue);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<boolean>(false);
 
-	let parsedItem;
+	useEffect(() => {
+		try {
+			setTimeout(() => {
+				const localeStorageItem = localStorage.getItem(itemName);
 
-	if (!localeStorageItem) {
-		localStorage.setItem(itemName, JSON.stringify(initialValue));
-		parsedItem = initialValue;
-	} else {
-		parsedItem = JSON.parse(localeStorageItem);
-	}
+				let parsedItem;
 
-	const [item, setItem] = useState(parsedItem);
+				if (!localeStorageItem) {
+					localStorage.setItem(itemName, JSON.stringify(initialValue));
+					parsedItem = initialValue;
+				} else {
+					parsedItem = JSON.parse(localeStorageItem);
+					setItem(parsedItem);
+				}
+				setLoading(false);
+			}, 1000);
+		} catch (error) {
+			setLoading(false);
+			setError(true);
+		}
+	}, []);
 
 	const saveItem = (newItem: TodoTypeData[]) => {
 		localStorage.setItem(itemName, JSON.stringify(newItem));
@@ -21,7 +34,7 @@ const useLocalStorage = (itemName: string, initialValue: TodoTypeData[]) => {
 		setItem(newItem);
 	};
 
-	return [item, saveItem];
+	return { item, saveItem, loading, error };
 };
 
 export { useLocalStorage };
