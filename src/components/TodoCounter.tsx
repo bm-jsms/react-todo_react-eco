22d60aabefa2dccfc9106876a6a4c16a071e144c
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-18 06:28:17 -0500

    ⚡️ [use] Context & count todo's

--- src/components/TodoCounter.tsx
+++ src/components/TodoCounter.tsx
@@ -1,12 +1,18 @@
-interface TodoCounterType {
-	completed: number;
-	total: number;
-}
+import { useContext } from 'react';
+import { TodoContext } from '../context';
+import { TodoTypeData } from '../interfaces';
 
-const TodoCounter = ({ completed, total }: TodoCounterType) => {
+const TodoCounter = () => {
+	const { todos }: any = useContext(TodoContext);
+
+	const totalTodos = todos.length;
+	const completedTodos = todos.filter(
+		(todo: TodoTypeData) => todo.completed,
+	).length;
+
 	return (
 		<h1 className='text-center text-4xl font-bold text-white mb-10'>
-			You're completed {completed} of {total} todo's
+			You're completed {completedTodos} of {totalTodos} todo's
 		</h1>
 	);
 };
