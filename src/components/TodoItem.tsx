5698400d2ca1485379dad226c3a0dfe3ae01a026
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-17 19:32:00 -0500

    ✨ [add] Icons

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,25 +1,29 @@
-import { TodoType } from '../interfaces';
+import { CompleteIcon } from '../icons';
+import { DeleteIcon } from '../icons/DeleteIcon';
+import type { TodoType } from '../interfaces';
 
 const TodoItem = ({ title, completed, onComplete, onDelete }: TodoType) => {
 	return (
 		<li className='flex items-center w-full border justify-between px-5'>
-			<span
+			{/* <span
 				className={`text-xl ${
 					completed && 'text-green-500'
 				} font-semibold cursor-pointer`}
 				onClick={onComplete}
 			>
 				V
-			</span>
+			</span> */}
 
+			<CompleteIcon handleClick={onComplete} />
 			<p className={`text-xl ${completed && 'line-through'}`}>{title}</p>
+			<DeleteIcon handleClick={onDelete} />
 
-			<span
+			{/* <span
 				className='text-xl cursor-pointer hover:text-red-600'
 				onClick={onDelete}
 			>
 				X
-			</span>
+			</span> */}
 		</li>
 	);
 };
