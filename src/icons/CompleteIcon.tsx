b855d1489c92a4a0e9661e3116e8bdaaef7440cb
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-17 20:25:34 -0500

    🌱 [add] completed Icons handle

--- src/icons/CompleteIcon.tsx
+++ src/icons/CompleteIcon.tsx
@@ -1,13 +1,17 @@
 import { IoCheckmarkCircleOutline } from 'react-icons/io5';
+import { IoMdCheckmarkCircle } from 'react-icons/io';
 
 import type { TodoIconHandle } from '../interfaces';
 
 const CompleteIcon = ({ handleClick, completed }: TodoIconHandle) => {
-	return (
+	return completed ? (
+		<IoMdCheckmarkCircle
+			className='cursor-pointer text-2xl duration-150 hover:scale-110 text-green-500 font-bold hover:text-green-700'
+			onClick={handleClick}
+		/>
+	) : (
 		<IoCheckmarkCircleOutline
-			className={`${
-				completed && 'text-green-500'
-			} cursor-pointer text-2xl duration-150 hover:scale-110 hover:text-green-500 font-bold`}
+			className='cursor-pointer text-2xl duration-150 hover:scale-110 hover:text-green-500 font-bold'
 			onClick={handleClick}
 		/>
 	);
