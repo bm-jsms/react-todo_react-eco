e15c553b04609c002f6b7ed0945c0630248473f4
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-18 02:24:53 -0500

    🚀 [export] appUI - types

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export type { TodoType } from './Todo.type';
 export type { TodoIconHandle } from './TodoIconHandle.type';
 export type { TodoTypeData } from './TodoTypeData';
+export type { AppUIProps } from './AppUI.type';
