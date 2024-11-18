c6f1f5aa667a28a408503ae8c6c62a41c628ecaa
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-18 02:24:31 -0500

    🏷️ [create] appUI types

---
+++ src/interfaces/AppUI.type.ts
@@ -0,0 +1,12 @@
+import { Dispatch } from 'react';
+import { TodoTypeData } from './TodoTypeData';
+
+export interface AppUIProps {
+	todos: TodoTypeData[];
+	searchedTodos: TodoTypeData[];
+	inputValue: string;
+
+	completeTodo: (text: string) => void;
+	deleteTodo: (text: string) => void;
+	setInputValue: Dispatch<React.SetStateAction<string>>;
+}
