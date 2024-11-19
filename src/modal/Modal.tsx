9e24b1151dd1be7c12098ff2731b7b6a9c8d6a7b
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-19 02:14:35 -0500

    💄 [up] Modal styles

--- src/modal/Modal.tsx
+++ src/modal/Modal.tsx
@@ -8,7 +8,7 @@ interface ModalProps {
 function Modal({ children }: ModalProps) {
 	return ReactDOM.createPortal(
 		<div className='fixed top-0 left-0 w-screen h-screen bg-black/50 flex items-center justify-center'>
-			<div className='bg-slate-400 z-50 w-full max-w-md p-4 rounded-md'>
+			<div className='bg-slate-600 z-50 w-1/4 max-w-md p-4 rounded-md shadow-slate-500 shadow-md'>
 				{children}
 			</div>
 		</div>,
