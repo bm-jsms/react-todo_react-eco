35b659f71e6a78b7c4e0d4d7f7b982d3d5702797
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-19 00:17:14 -0500

    ⚡️ [use] context

--- src/components/TodoSearch.tsx
+++ src/components/TodoSearch.tsx
@@ -1,24 +1,21 @@
-import { Dispatch } from 'react';
+import { useContext } from 'react';
+import { TodoContext } from '../context';
 
-interface TodoSearchType {
-	searchValue: string;
-	setSearchValue: Dispatch<React.SetStateAction<string>>;
-}
-
-const TodoSearch = ({ searchValue, setSearchValue }: TodoSearchType) => {
+const TodoSearch = () => {
+	const { inputValue, setInputValue }: any = useContext(TodoContext);
 	return (
 		<>
 			<input
 				type='text'
-				value={searchValue}
+				value={inputValue}
 				onChange={(e) => {
-					setSearchValue(e.target.value);
+					setInputValue(e.target.value);
 				}}
 				placeholder='Type to search'
 				className='w-2/4 bg-transparent rounded-md border py-[10px] px-5 text-dark-6 outline-none transition focus:border-blue-500  active:border-primary focus:bg-slate-700 border-blue-400 bg-slate-800'
 			/>
 
-			<p>Current Value: {searchValue}</p>
+			<p>Current Value: {inputValue}</p>
 		</>
 	);
 };
