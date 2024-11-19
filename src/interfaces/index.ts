bf543272bb2ca98870cbc62b43b0c7d7023cf332
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-19 01:29:00 -0500

    🚀 [export] TodoContext type

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,4 @@ export type { TodoType } from './Todo.type';
 export type { TodoIconHandle } from './TodoIconHandle.type';
 export type { TodoTypeData } from './TodoTypeData';
 export type { AppUIProps } from './AppUI.type';
+export type { TodoContextType } from './TodoContext.type';
