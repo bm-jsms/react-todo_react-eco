b1f302be5cadb37f065118738ae26b2ac4e6aa04
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-19 02:13:12 -0500

    🔥 [rm] initial data

--- src/data/Todos.ts
+++ src/data/Todos.ts
@@ -1,21 +1,5 @@
 import type { TodoTypeData } from '../interfaces';
 
-const defaultTodos: TodoTypeData[] = [
-	{
-		id: 1,
-		title: 'Todo 1',
-		completed: false,
-	},
-	{
-		id: 2,
-		title: 'Todo 2',
-		completed: true,
-	},
-	{
-		id: 3,
-		title: 'Todo 3',
-		completed: false,
-	},
-];
+const defaultTodos: TodoTypeData[] = [];
 
 export { defaultTodos };
